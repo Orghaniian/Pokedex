ffee74f7452d6e77c1d0ab45ae5bfaecf0c596db
Julien Boulvert <julien.boulvert@frog.co>
2022-12-29 21:50:40 +0100

    optimization: fetch pokemon details in parallel

--- app/src/main/java/com/orghaniian/pokedex/data/remote/PokemonRemoteDataSource.kt
+++ app/src/main/java/com/orghaniian/pokedex/data/remote/PokemonRemoteDataSource.kt
@@ -4,14 +4,19 @@ import com.orghaniian.pokedex.data.local.Pokemon
 import com.orghaniian.pokedex.data.model.Color
 import com.orghaniian.pokedex.data.model.Type
 import com.orghaniian.pokedex.data.remote.model.GetAllResponse
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 
 class PokemonRemoteDataSource @Inject constructor(private val pokeApiService: PokeApiService) {
     suspend fun getAllPokemon(limit: Int? = null, offset: Int? = null) = pokeApiService.run {
         val apiResponse = getPokemon(limit, offset)
 
-        val pokemons = apiResponse.results.map { (name, _) ->
-            fetchPokemon(name)
+        val pokemons = coroutineScope {
+            apiResponse.results.map { (name, _) ->
+                async { fetchPokemon(name) }
+            }.awaitAll()
         }
 
         return@run GetAllResponse(
