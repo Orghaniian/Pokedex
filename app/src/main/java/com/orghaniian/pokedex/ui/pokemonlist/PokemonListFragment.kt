e3d261681ae0c5205f278325dae5af6a9426e665
Julien Boulvert <julien.boulvert@frog.co>
2022-12-29 21:40:20 +0100

    remove load indicators

--- app/src/main/java/com/orghaniian/pokedex/ui/pokemonlist/PokemonListFragment.kt
+++ app/src/main/java/com/orghaniian/pokedex/ui/pokemonlist/PokemonListFragment.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.paging.LoadState.Loading
 import androidx.recyclerview.widget.GridLayoutManager
 import com.orghaniian.pokedex.databinding.FragmentPokemonListBinding
 import com.orghaniian.pokedex.domain.FormatNameUseCase
@@ -53,14 +51,6 @@ class PokemonListFragment : Fragment() {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.pagingData.collectLatest { pagingData ->
                     pokemonListAdapter.submitData(pagingData)
-                }
-            }
-        }
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                pokemonListAdapter.loadStateFlow.collect {
-                    binding.prependProgress.isVisible = it.source.prepend is Loading
-                    binding.appendProgress.isVisible = it.source.append is Loading
                 }
             }
         }
